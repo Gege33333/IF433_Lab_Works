4fd00b9b2a6af4dfab59c14cd5dcdb50b80081cf
Gege33333 <gerfed33@gmail.com>
2026-02-04 12:22:56 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_113652_GeradFedorian/week01/GameStore.kt
+++ src/oop_113652_GeradFedorian/week01/GameStore.kt
@@ -1,6 +1,16 @@
 package oop_113652_GeradFedorian.week01
 
 fun main(){
-    val gameTitle: String = ""
-    val price: Int = 0
+    val gameTitle: String = "Valorant"
+    var price: Int = 300000
+
+    val discount = calculateDiscount(price)
+}
+
+fun calculateDiscount(price: Int): Int {
+    return if (price > 500000) {
+        price * 20 / 100
+    } else {
+        price * 10 / 100
+    }
 }
