2ebf0fdc64ef21222a02072e51a9939ea55f6c29
Gege33333 <gerfed33@gmail.com>
2026-02-20 15:20:18 +0700

    week03: simulate stack overflow error in setter

--- src/oop_113652_GeraldFedorian/week03/Tugas/Employee.kt
+++ src/oop_113652_GeraldFedorian/week03/Tugas/Employee.kt
@@ -2,4 +2,6 @@ package oop_113652_GeraldFedorian.week03.Tugas
 
 class Employee {
     var salary: Int = 0
+    println("Mencoba set gaji ke: $value")
+    this.salary = value
 }
---
+++ src/oop_113652_GeraldFedorian/week03/Tugas/Main.kt
@@ -0,0 +1,7 @@
+package oop_113652_GeraldFedorian.week03.Tugas
+
+fun main{
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
