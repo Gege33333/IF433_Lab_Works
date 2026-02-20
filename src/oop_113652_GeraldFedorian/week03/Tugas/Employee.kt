5a3501f1fc47036edb0730549b7dd9e68577bbda
Gege33333 <gerfed33@gmail.com>
2026-02-20 15:22:12 +0700

    week03: fix setter using backing field

--- src/oop_113652_GeraldFedorian/week03/Tugas/Employee.kt
+++ src/oop_113652_GeraldFedorian/week03/Tugas/Employee.kt
@@ -2,6 +2,12 @@ package oop_113652_GeraldFedorian.week03.Tugas
 
 class Employee {
     var salary: Int = 0
-    println("Mencoba set gaji ke: $value")
-    this.salary = value
+        set(value) {
+            if(value <0){
+                println("Error: agji tidak boleh negatif")
+                field = 0
+            } else{
+                field = value
+            }
+        }
 }
