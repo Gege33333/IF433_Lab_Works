dfea4b0450fd004ea317d764c7ddb3b35f447063
Gege33333 <gerfed33@gmail.com>
2026-03-10 15:47:06 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_113652_GeraldFedorian/week06/TugasMandiri/Main.kt
@@ -0,0 +1,9 @@
+package oop_113652_GeraldFedorian.week06.TugasMandiri
+
+import oop_113652_GeraldFedorian.week06.LatihanTerbimbing.Smartwatch
+
+fun main(){
+    val lamp = SmartLamp("1" ,"Ruang Tamu")
+    val speaker = SmartSpeaker("1", "Google Nest Dapur")
+    val cctv = SmartCCTV("1", "Ezviz Garasi")
+}
