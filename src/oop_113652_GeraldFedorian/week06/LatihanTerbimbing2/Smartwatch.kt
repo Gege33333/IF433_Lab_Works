46e884fc6a305c2b8fad7b20a2bb2ac4c6603be0
Gege33333 <gerfed33@gmail.com>
2026-03-10 14:59:00 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_113652_GeraldFedorian/week06/LatihanTerbimbing2/Smartwatch.kt
@@ -0,0 +1,15 @@
+package oop_113652_GeraldFedorian.week06.LatihanTerbimbing2
+
+class Smartwatch: Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun ChargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
