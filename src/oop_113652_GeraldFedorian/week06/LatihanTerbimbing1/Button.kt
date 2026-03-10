9455ef20c53778af664eb0895b3f290cc78aad20
Gege33333 <gerfed33@gmail.com>
2026-03-10 14:53:46 +0700

    week06: create Watch abstract class

--- src/oop_113652_GeraldFedorian/week06/LatihanTerbimbing1/Button.kt
+++ src/oop_113652_GeraldFedorian/week06/LatihanTerbimbing1/Button.kt
@@ -1,4 +1,4 @@
-package oop_113652_GeraldFedorian.week06.LatihanTerbimbing
+package oop_113652_GeraldFedorian.week06.LatihanTerbimbing1
 
 class Button(override val name: String): Clickable {
     override fun click() {
--- src/oop_113652_GeraldFedorian/week06/LatihanTerbimbing1/Clickable.kt
+++ src/oop_113652_GeraldFedorian/week06/LatihanTerbimbing1/Clickable.kt
@@ -1,4 +1,4 @@
-package oop_113652_GeraldFedorian.week06.LatihanTerbimbing
+package oop_113652_GeraldFedorian.week06.LatihanTerbimbing1
 
 interface Clickable{
     val name: String
