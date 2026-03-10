462d5e3b336fdae873ecfe19cad664cc780b3ed7
Gege33333 <gerfed33@gmail.com>
2026-03-10 14:55:09 +0700

    week06: create compound interfaces

---
+++ src/oop_113652_GeraldFedorian/week06/LatihanTerbimbing2/Capabilities.kt
@@ -0,0 +1,9 @@
+package oop_113652_GeraldFedorian.week06.LatihanTerbimbing2
+
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable{
+    fun ChargeBattery()
+}
