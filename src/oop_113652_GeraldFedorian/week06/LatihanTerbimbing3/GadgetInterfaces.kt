12b06fc6c05cca3cd0f81128cdddd61bf71ec175
Gege33333 <gerfed33@gmail.com>
2026-03-10 15:00:53 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_113652_GeraldFedorian/week06/LatihanTerbimbing3/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_113652_GeraldFedorian.week06.LatihanTerbimbing3
+
+interface Camera {
+    fun tunrOn(){
+        println("Lensa Kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
