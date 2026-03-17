78ba537fa5cb0d5491ed4c997321ba5279e0e74b
Gege33333 <gerfed33@gmail.com>
2026-03-17 15:58:50 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_113652_GeraldFedorian/week07/TugasMandiri/Main.kt
+++ src/oop_113652_GeraldFedorian/week07/TugasMandiri/Main.kt
@@ -1,6 +1,19 @@
 package oop_113652_GeraldFedorian.week07.TugasMandiri
 
 fun main() {
+
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n=== SIMULASI FACTORY & ENUM ===")
+
+    println("Drop Chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    val starterWeapon = Weapon.forgeStarterSword()
+
+    println("\nSenjata Awal Pemain:")
+    println("Nama: ${starterWeapon.item.name}")
+    println("Damage: ${starterWeapon.item.damage}")
+    println("Rarity: ${starterWeapon.item.rarity}")
+    println("Durability: ${starterWeapon.durability}")
 }
