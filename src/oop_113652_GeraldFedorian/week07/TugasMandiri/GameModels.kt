5d286227ba5c58a4680788725e7094f0896cfd83
Gege33333 <gerfed33@gmail.com>
2026-03-17 15:45:25 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_113652_GeraldFedorian/week07/TugasMandiri/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_113652_GeraldFedorian.week07.TugasMandiri
+
+enum class ItemRarity(val dropChance: Int){
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
