776b3b32ef9b426ae9ccb7c6485e4cf432d78c9b
Gege33333 <gerfed33@gmail.com>
2026-04-14 15:12:29 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_113652_GeraldFedorian/week08/LatihanTerbimbing/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_113652_GeraldFedorian.week08.LatihanTerbimbing;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus(){
+        return "SERVER_ONLINE_200_OK";
+    }
+}
