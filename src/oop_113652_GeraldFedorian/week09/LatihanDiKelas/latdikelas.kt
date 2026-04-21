5890d499bde2daed774a4d6aed6bedf0606cbf64
Gege33333 <gerfed33@gmail.com>
2026-04-21 13:34:04 +0700

    week09: implement map for key-value storage

--- src/oop_113652_GeraldFedorian/week09/LatihanDiKelas/latdikelas.kt
+++ src/oop_113652_GeraldFedorian/week09/LatihanDiKelas/latdikelas.kt
@@ -1,9 +1,29 @@
 package oop_113652_GeraldFedorian.week09.LatihanDiKelas
 
 fun main(){
+    println("===== LIST OF =====")
     var arMhs = listOf("Budi", "susi", "gege")
-
     println(arMhs)
     println("panjang List ${arMhs.size}")
     println(arMhs[1])
+    for(a in arMhs){
+        println("Mahasiswa bernama: $a")
+    }
+
+    println("==== MUTABLE LIST ====")
+    var arAngka = mutableListOf(10,30,40,20,90,100,15)
+    println(arAngka)
+
+    //nambah array
+    arAngka.add(777)
+
+    //ganti atau replace array
+    arAngka[2] = 123
+
+    //hapus array
+    arAngka.removeAt(3)
+
+    //view hasil
+    println(arAngka)
+
 }
