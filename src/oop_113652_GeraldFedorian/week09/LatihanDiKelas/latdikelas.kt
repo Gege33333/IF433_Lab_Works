1009dfd1cdebcc79f24d4d28fe6ad29bc0044e1e
Gege33333 <gerfed33@gmail.com>
2026-04-21 13:25:10 +0700

    week09: implement set to discard duplicate elements

---
+++ src/oop_113652_GeraldFedorian/week09/LatihanDiKelas/latdikelas.kt
@@ -0,0 +1,9 @@
+package oop_113652_GeraldFedorian.week09.LatihanDiKelas
+
+fun main(){
+    var arMhs = listOf("Budi", "susi", "gege")
+
+    println(arMhs)
+    println("panjang List ${arMhs.size}")
+    println(arMhs[1])
+}
