3b33a7d231ceae6f2c99aa3122095f0df4b6ad79
Gege33333 <gerfed33@gmail.com>
2026-04-28 15:19:04 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_113652_GeraldFedorian/week10/TugasMandiri/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_113652_GeraldFedorian.week10.TugasMandiri
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
