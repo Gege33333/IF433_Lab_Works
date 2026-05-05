2f29ec31b8ecd4cbde1bd1507ea6f64f3d4d858f
Gege33333 <gerfed33@gmail.com>
2026-05-05 14:53:31 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_113652_GeraldFedorian/week11/TugasMandiri/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_113652_GeraldFedorian.week11.TugasMandiri
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
