73a2ef527bafc1cca4f6f3cd035fb11518d72bc0
Gege33333 <gerfed33@gmail.com>
2026-05-12 15:07:25 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_113652_GeraldFedorian/week12/Tugas/SmartFeeder.kt
+++ src/oop_113652_GeraldFedorian/week12/Tugas/SmartFeeder.kt
@@ -16,5 +16,20 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 fun main() {
     println("=== SMART PET FEEDER SYSTEM ===")
     var currentKibbleStock = 50
+
+    println("\n--- Jadwal Makan Pagi ---")
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+    } catch (e: DispenserJamException) {
+        println("ERROR HARDWARE: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("ERROR STOK: ${e.message}")
+    } catch (e: Exception) {
+        println("ERROR UMUM: ${e.message}")
+    }
 }
 
