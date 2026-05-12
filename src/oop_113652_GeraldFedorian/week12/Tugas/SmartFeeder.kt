5638dfd08bf1b44f9dab87cc34d418b6f14dd336
Gege33333 <gerfed33@gmail.com>
2026-05-12 15:02:08 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_113652_GeraldFedorian/week12/Tugas/SmartFeeder.kt
@@ -0,0 +1,6 @@
+package oop_113652_GeraldFedorian.week12.Tugas
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+}
+
