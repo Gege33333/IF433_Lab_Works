d428ffcef2a776f8a0b5411b84cce9cc1f428ab2
Gege33333 <gerfed33@gmail.com>
2026-05-12 14:53:59 +0700

    week12: create basic try-catch-finally

---
+++ src/oop_113652_GeraldFedorian/week12/Tugas/BasicException.kt
@@ -0,0 +1,12 @@
+package oop_113652_GeraldFedorian.week12.Tugas
+
+fun divide(a: Int, b: Int): Int {
+    try {
+        return a / b
+    } catch (e: ArithmeticException) {
+        println("Error: ${e.message}")
+        return -1
+    } finally {
+        println("Division attempt finished")
+    }
+}
