9f7bca8744596f7033109900c2f9511592846c17
Gege33333 <gerfed33@gmail.com>
2026-05-19 14:59:31 +0700

    week13: create Student model

---
+++ src/oop_113652_GeraldFedorian/week13/LatihanTerbimbing/StudentSerialization.kt
@@ -0,0 +1,6 @@
+package oop_113652_GeraldFedorian.week13.LatihanTerbimbing
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(val name: String, val age: Int, val gpa: Double)
